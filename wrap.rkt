c35aa01804671aefaead152477548cb7ff1d8aa8
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-27 14:05:54 +0800

    rename

--- wrap.rkt
+++ wrap.rkt
@@ -2,7 +2,7 @@
 (require racket/control
          (for-syntax syntax/parse))
 
-(define log-tag (make-continuation-prompt-tag 'log))
+(define effect-log (make-continuation-prompt-tag 'log))
 (define-syntax (with stx)
   (syntax-parse stx
     [(with [tag handler] body)
@@ -12,7 +12,7 @@
 
 (define (log msg)
   (call/cc (λ (k)
-             (abort/cc log-tag k msg))))
+             (abort/cc effect-log k msg))))
 
 (define (f)
   (println 1)
@@ -21,7 +21,7 @@
   (log 4)
   (println 5))
 
-(with [log-tag (λ (resume v)
+(with [effect-log (λ (resume v)
                  (println v)
                  (resume))]
       (f))
