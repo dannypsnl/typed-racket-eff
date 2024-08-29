b9d4bb48852c736c1ab07c3b0b8fcd0411e42967
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-29 21:39:38 +0800

    let effect & define/eff use macro
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- macro.rkt
+++ macro.rkt
@@ -1,24 +1,33 @@
 #lang typed/racket
-(require (for-syntax syntax/parse))
+(require (for-syntax syntax/parse
+                     syntax/stx))
 
-(begin
-  #;(begin-for-syntax
-      ())
-
-  (: effect-log : (Prompt-Tagof Any (-> (-> Number Void) Number Void)))
-  (define effect-log (make-continuation-prompt-tag 'tag)))
+(define-syntax effect
+  (syntax-parser
+    #:datum-literals (:)
+    [(_ name:id : T)
+     (syntax-parse #'T
+       [(-> In ... Out)
+        #'(begin
+            (begin-for-syntax
+              (define name #'T))
+            (define name : (Prompt-Tagof Any
+                                         (-> (-> Out Void) In ... Void))
+              (make-continuation-prompt-tag 'tag)))])]))
+(effect log : (-> Number Number))
 
 (define-syntax define/eff
   (syntax-parser
     #:datum-literals (:)
-    [(_ (f:id [x:id : T] ...) : T_out {[effs:id : T*] ...}
+    [(_ (f:id [x:id : T] ...) : T_out {eff*:id ...}
         body*:expr ...+ body:expr)
-     (println #'(effs ...))
-     #'(define ((f [effs : T*] ...) [x : T] ...) : T_out
-         body* ... body
-         )
-     ]))
-(define/eff (f [x : String]) : Void { [log : (-> Number Number)] }
+     (define bind*
+       (stx-map (λ (e)
+                  (define sign (eval e))
+                  #`[#,e : #,sign]) #'(eff* ...)))
+     #`(define ((f #,@bind*) [x : T] ...) : T_out
+         body* ... body)]))
+(define/eff (f [x : String]) : Void { log }
   (println 1)
   (println (log 2))
   (println 3)
@@ -38,17 +47,16 @@
                  -> Void)])
 
 
-(let ([log (λ ([n : Number])
-             (cast
-              (call/cc (λ ([k : (-> Number Void)])
-                         (abort/cc effect-log k n)))
-              Number))])
-  (call/prompt (λ () ((f log) "Hello"))
-               effect-log
+(let ([log-wrapper (λ ([n : Number])
+                     (cast
+                      (call/cc (λ ([k : (-> Number Void)])
+                                 (abort/cc log k n)))
+                      Number))])
+  (call/prompt (λ () ((f log-wrapper) "Hello"))
+               log
                (λ ([resume : (-> Number Void)]
                    [v : Number]) : Void
                  (println v)
                  (resume 10))
                )
   )
-
