be09843376568fd8f9b882d0fac134551416f355
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-29 12:49:34 +0800

    failed
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- eff/base.rkt
+++ eff/base.rkt
@@ -1,6 +1,5 @@
 #lang turnstile+
 (provide 
-  def
   #%datum #%app λ
   #%module-begin #%top-interaction
   (type-out Void Number String →))
@@ -17,27 +16,22 @@
    [⊢ (#%datum- . x) ⇒ Number]]
   [(_ . x:string) ≫
    -----
-   [⊢ (#%datum- . x) ⇒ String]]
-  )
+   [⊢ (#%datum- . x) ⇒ String]])
 ; [LAM]
 (define-typed-syntax (λ ([x:id : τ_in:type] ...) e) ≫
   [[x ≫ x- : τ_in.norm] ... ⊢ e ≫ e- ⇒ τ_out]
   -------
   [⊢ (λ- (x- ...) e-) ⇒ (→ τ_in.norm ... τ_out)])
 ; [APP]
-(define-typed-syntax (#%app e_fn e_arg ...) ≫
-  [⊢ e_fn ≫ e_fn- ⇒ (~→ τ_in ... τ_out)]
-  [⊢ e_arg ≫ e_arg- ⇐ τ_in] ...
-  --------
-  [⊢ (#%app- e_fn- e_arg- ...) ⇒ τ_out])
-
-(define-syntax def
-  (syntax-parser #:datum-literals (:)
-    [(_ (f:id [x:id : τ_in] ...) : τ_out
-        e)
-     #'(begin
-         (define-typed-variable-rename f ≫ f- : (→ τ_in ... τ_out))
-         (define- f- (λ- (x ...) e)))]))
+(define-typed-syntax #%app
+  [(_ void) ≫
+    -----
+    [⊢ (#%app- void-) ⇒ Void]]
+  [(_ e_fn e_arg ...) ≫
+    [⊢ e_fn ≫ e_fn- ⇒ (~→ τ_in ... τ_out)]
+    [⊢ e_arg ≫ e_arg- ⇐ τ_in] ...
+    --------
+    [⊢ (#%app- e_fn- e_arg- ...) ⇒ τ_out]])
 
 (module+ main
   ((λ ([x : Number]) x) 1)
